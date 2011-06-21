f93f77f7cc77ba1b55bb6e67bea96c43b16b1cb6
shrav mehta <shravvmehtaa@gmail.com>
2011-06-21 13:58:02 -0700

    Fixed Orientation, Added Admob SDK to Resources

---
+++ Candy Cat/src/com/embedstudios/candycat/CandyObject.java
@@ -0,0 +1,37 @@
+package com.embedstudios.candycat;
+
+import android.graphics.Picture;
+
+/*
+ * Class created by Prem.
+ * 
+ * This class will be the template class for all of the game objects,
+ * such as the Candyman or the Candy.
+ * 
+ * It will require different attributes that describe the game objects, like gravity;
+ */
+public class CandyObject {
+
+	private boolean isSubjectToGravity;
+	private int rowPos,columnPos;
+	private Picture picture;
+
+	public CandyObject () {
+		isSubjectToGravity = false;
+		rowPos=0;
+		columnPos=0;
+		picture=null;
+	}
+	
+	public CandyObject(int inputRow,int inputColumn,boolean inputGravity,Picture inputPicture) {
+		isSubjectToGravity=inputGravity;
+		rowPos=inputRow;
+		columnPos=inputColumn;
+		picture=inputPicture;
+	}
+	
+	public int getRow() {return rowPos;}
+	public int getColumn() {return columnPos;}
+	public void setRow(int inputRow) {rowPos=inputRow;}
+	public void setColumn(int inputColumn) {rowPos=inputColumn;}
+}
