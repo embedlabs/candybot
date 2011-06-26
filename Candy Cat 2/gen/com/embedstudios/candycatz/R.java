58a22793ced4984cda29fe024eb7bd6f00fc7aa0
shrav mehta <shravvmehtaa@gmail.com>
2011-06-26 00:16:46 -0700

    Added a thread to loading to open the menu

--- Candy Cat 2/gen/com/embedstudios/candycatz/R.java
+++ Candy Cat 2/gen/com/embedstudios/candycatz/R.java
@@ -14,15 +14,18 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050003;
         public static final int linearLayout1=0x7f050000;
         public static final int progressBar1=0x7f050002;
         public static final int textView1=0x7f050001;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int load=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int load=0x7f040001;
+        public static final int play=0x7f040002;
     }
 }
--- Candy Cat 2/src/com/embedstudios/candycatz/CandyCatMainActivity.java
+++ Candy Cat 2/src/com/embedstudios/candycatz/CandyCatMainActivity.java
@@ -1,6 +1,10 @@
 package com.embedstudios.candycatz;
 
+
+
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CandyCatMainActivity extends Activity {
@@ -8,6 +12,42 @@ public class CandyCatMainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.load);
+		//startActivity(new Intent(this, CandyCatMenuActivity.class)); // starting level select screen
+
+		 //your code setContentView() etc....
+        Thread toRun = new Thread()
+        {
+               public void run()
+               { try {
+            	   Thread.sleep(2000);
+               } catch (InterruptedException e) {
+               }
+              
+                      Intent intent = new Intent (CandyCatMainActivity.this, CandyCatMenuActivity.class);
+                      startActivity(intent);
+               }
+        };
+        toRun.start();
+
+
+
+	/*	thread = new Thread()
+		{
+		    @Override
+		    public void run() {
+		        try {
+		            while(true) {
+		                
+		            }
+		        } catch (InterruptedException e) {
+		            e.printStackTrace();
+		        }
+		    }
+		};
+*/
+        
+        
     }
 }
+
