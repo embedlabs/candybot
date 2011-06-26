66ac92b6c28a48e872fc3042c3412136957b3534
Shrav Mehta <shravvmehtaa@gmail.com>
2011-06-26 15:37:58 -0700

    Embed Logo one image will change when finalized

--- Candy Cat 2/gen/com/embedstudios/candycatz/R.java
+++ Candy Cat 2/gen/com/embedstudios/candycatz/R.java
@@ -23,11 +23,11 @@ public final class R {
         public static final int icon=0x7f020000;
         public static final int menubg=0x7f020001;
         public static final int playbutton=0x7f020002;
+        public static final int splash=0x7f020003;
     }
     public static final class id {
         public static final int button1=0x7f060002;
         public static final int linearLayout1=0x7f060000;
-        public static final int progressBar1=0x7f060003;
         public static final int textView1=0x7f060001;
     }
     public static final class layout {
