956e444f46ab630b1125604486e9d9aa14dad57a
Prem Nair <prem.q.nair@gmail.com>
2011-07-15 19:58:10 -0700

    Made everything render correctly, animation frames playing just to show that they can (frames are playing in incorrect order, this is intentional so I can cycle through them all). I updated my Android SDK and ADT Plugin but still have Helios. Will now begin work on changing main screen to 1-button play, and creating a popup menu with the capacitive phone button. Shrav, pngcrush all images in the project except the bundle folder, and all images in the Achievements folder which is under the 64x64 PNGs folder. Weeee.

--- Candy Cat/src/com/embedstudios/candycat/CandyLevel.java
+++ Candy Cat/src/com/embedstudios/candycat/CandyLevel.java
@@ -15,18 +15,18 @@ import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXLayer;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXLoader;
+import org.anddev.andengine.entity.layer.tiled.tmx.TMXLoader.ITMXTilePropertiesListener;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXProperties;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXTile;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXTileProperty;
 import org.anddev.andengine.entity.layer.tiled.tmx.TMXTiledMap;
 import org.anddev.andengine.entity.layer.tiled.tmx.util.exception.TMXLoadException;
-import org.anddev.andengine.entity.layer.tiled.tmx.TMXLoader.ITMXTilePropertiesListener;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.BaseSprite;
 import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.entity.util.FPSLogger;
-import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
 import org.anddev.andengine.opengl.texture.Texture;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.region.TextureRegion;
@@ -34,8 +34,6 @@ import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 import org.anddev.andengine.util.Debug;
-
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 import android.util.Log;
 import android.view.Display;
@@ -63,11 +61,14 @@ public class CandyLevel extends BaseGameActivity implements ITMXTilePropertiesLi
 	private BoundCamera mBoundChaseCamera;
 	
 	private Texture mOnScreenControlTexture;
+	private TextureRegion mOnScreenControlBaseTextureRegion, mOnScreenControlKnobTextureRegion;
+	
 	private Texture mTexturePlayer;
-	private Texture mTexture;
 	private TiledTextureRegion mPlayerTextureRegion;
-	private TextureRegion mOnScreenControlBaseTextureRegion, mIceFaceTextureRegion, mCandyFaceTextureRegion, mCatFaceTextureRegion, mWallFaceTextureRegion, mBoxFaceTextureRegion, mInertiaFaceTextureRegion;
-	private TextureRegion mOnScreenControlKnobTextureRegion;
+	
+	private Texture mObjectTexture;
+	private TextureRegion boxTR, movableWallTR, inertiaWallTR;
+	private TiledTextureRegion candyTTR, catTTR, bombTTR, enemyTTR;
 
 	private DigitalOnScreenControl mDigitalOnScreenControl;
 
@@ -98,14 +99,14 @@ public class CandyLevel extends BaseGameActivity implements ITMXTilePropertiesLi
 		/**
 		 * If you want to see actual size.
 		 */
-		//mBoundChaseCamera = new BoundCamera(0,0,PHONE_WIDTH,PHONE_HEIGHT,0,WIDTH,0,HEIGHT);
-		//final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(PHONE_WIDTH, PHONE_HEIGHT), mBoundChaseCamera);
+		mBoundChaseCamera = new BoundCamera(0,0,PHONE_WIDTH,PHONE_HEIGHT,0,WIDTH,0,HEIGHT);
+		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(PHONE_WIDTH, PHONE_HEIGHT), mBoundChaseCamera);
 
 		/**
 		 * If you want to see the whole level.
 		 */
-		mBoundChaseCamera = new BoundCamera(0,0,WIDTH,HEIGHT);
-		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(WIDTH, HEIGHT), mBoundChaseCamera);
+//		mBoundChaseCamera = new BoundCamera(0,0,WIDTH,HEIGHT);
+//		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(WIDTH, HEIGHT), mBoundChaseCamera);
 		
 		return new Engine(engineOptions);
 	}
@@ -114,25 +115,27 @@ public class CandyLevel extends BaseGameActivity implements ITMXTilePropertiesLi
 	public void onLoadResources() {
 		Log.i(TAG,"CandyLevel onLoadResources()");
 		TextureRegionFactory.setAssetBasePath("gfx/");
-		mTexture = new Texture(512,64, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		mTexturePlayer = new Texture(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
-		mOnScreenControlTexture = new Texture(256, 128, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		
+		mOnScreenControlTexture = new Texture(256, 128, TextureOptions.NEAREST_PREMULTIPLYALPHA);
 		mOnScreenControlBaseTextureRegion = TextureRegionFactory.createFromAsset(mOnScreenControlTexture, this, "onscreen_control_base.png", 0, 0);
 		mOnScreenControlKnobTextureRegion = TextureRegionFactory.createFromAsset(mOnScreenControlTexture, this, "onscreen_control_knob.png", 128, 0);
-		mPlayerTextureRegion = TextureRegionFactory.createTiledFromAsset(mTexturePlayer, this, "candy.png",0,0,4,3);
-		mEngine.getTextureManager().loadTextures(mOnScreenControlTexture,mTexturePlayer, mTexture);
-		
-		mCandyFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "full_candy.png",0,0);
-		mCatFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "cat.png", 320,0);
-		mIceFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "ice.png",260,0);
-		mBoxFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "box.png",130,0);
-		mWallFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "movable_wall.png",65,0);
-		mInertiaFaceTextureRegion = TextureRegionFactory.createFromAsset(mTexture, this, "inertia_wall.png",195,0);
 
-		// private static final int BOMB = 3;
-		// private static final int ENEMY = 4;
-	
+		mTexturePlayer = new Texture(256, 256, TextureOptions.NEAREST_PREMULTIPLYALPHA);
+		mPlayerTextureRegion = TextureRegionFactory.createTiledFromAsset(mTexturePlayer, this, "candy.png",0,0,4,3);
 		
+
+		mObjectTexture = new Texture(512,512, TextureOptions.NEAREST_PREMULTIPLYALPHA);
+		candyTTR = TextureRegionFactory.createTiledFromAsset(mObjectTexture, this, "candy.png",0,0,4,3); // done
+		catTTR = TextureRegionFactory.createTiledFromAsset(mObjectTexture, this, "cat.png", 256,0,4,2); // done
+		boxTR = TextureRegionFactory.createFromAsset(mObjectTexture, this, "box.png",0,192); // done
+		bombTTR = TextureRegionFactory.createTiledFromAsset(mObjectTexture, this, "bomb.png",256,128,4,2);
+		enemyTTR = TextureRegionFactory.createTiledFromAsset(mObjectTexture, this, "enemy.png",256,256,4,2);
+		movableWallTR = TextureRegionFactory.createFromAsset(mObjectTexture, this, "movable_wall.png",64,192);
+		inertiaWallTR = TextureRegionFactory.createFromAsset(mObjectTexture, this, "inertia_wall.png",128,192);
+
+		mEngine.getTextureManager().loadTextures(mOnScreenControlTexture,mTexturePlayer, mObjectTexture);
+
+		CandyUtils.parseLevelObjectsFromXml(this, world, level, objectList, tutorialList);
 	}
 
 	@Override
@@ -165,84 +168,52 @@ public class CandyLevel extends BaseGameActivity implements ITMXTilePropertiesLi
 		/**
 		 * SPRITES
 		 */
-		CandyUtils.parseLevelObjectsFromXml(this, world, level, objectList, tutorialList); // TODO
 		for (int[] i:objectList) {
 			createSprite(i[0],i[1],i[2]);
 		}
-		for (String text:tutorialList) {
-			addTutorialText(text);
-		}
-		
-		
-		// Have the camera follow the player
-//		final int centerX = (PHONE_WIDTH - mPlayerTextureRegion.getWidth()) / 2;
-//		final int centerY = (PHONE_HEIGHT - mPlayerTextureRegion.getHeight()) / 2;
-		/* Create the sprite and add it to the scene. */
-		final AnimatedSprite candy = new AnimatedSprite(64*21, 64*2, mPlayerTextureRegion);
-		candy.setRotation(0.5f); // TODO why is this needed?
-		candy.animate(100,true);
-		mBoundChaseCamera.setChaseEntity(candy);
-		final PhysicsHandler physicsHandler = new PhysicsHandler(candy);
-		candy.registerUpdateHandler(physicsHandler);
-		mScene.attachChild(candy);
-
-		// Control onscreen control
-		mDigitalOnScreenControl = new DigitalOnScreenControl(32, PHONE_HEIGHT - mOnScreenControlBaseTextureRegion.getHeight()-32, mBoundChaseCamera, mOnScreenControlBaseTextureRegion, mOnScreenControlKnobTextureRegion, 0.1f, new IOnScreenControlListener() {
-			@Override
-			public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl, final float pValueX, final float pValueY) {
-				if (pValueY == 1) {
-					playerDirection = PlayerDirection.UP;
-				} else if (pValueY == -1) {
-					playerDirection = PlayerDirection.DOWN;
-				} else if (pValueX == -1) {
-					playerDirection = PlayerDirection.LEFT;
-				} else if (pValueX == 1) {
-					playerDirection = PlayerDirection.RIGHT;
-				}
-				physicsHandler.setVelocity(pValueX * 64, pValueY * 64);
-			}
-		});
-		
-		// Add onscreen control
-		mDigitalOnScreenControl.getControlBase().setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
-		mDigitalOnScreenControl.getControlBase().setAlpha(0.5f);
-		mDigitalOnScreenControl.getControlBase().setScaleCenter(0, 128);
-		mDigitalOnScreenControl.getControlBase().setScale(1.25f);
-		mDigitalOnScreenControl.getControlKnob().setScale(1.25f);
-		mDigitalOnScreenControl.refreshControlKnobPosition();
-
-		mScene.setChildScene(mDigitalOnScreenControl);
+		addTutorialText(tutorialList);
 		
 		return mScene;
 	}
 
-	private void addTutorialText(String text) {
+	private void addTutorialText(ArrayList<String> inputList) {
 		// TODO Auto-generated method stub
 	}
 
-	private void createSprite(int type, int row, int column) {
-		final Sprite face;
-		row = row * 64;
-		column = column * 64;
+	private void createSprite(final int type, int row, int column) {
+		final BaseSprite face;
+		row *= 64;
+		column *= 64;
 		
 		switch (type){
-		case 0:
-			face = new Sprite(row,column,mCandyFaceTextureRegion);
+		case CANDY: // done
+			face = new AnimatedSprite(column,row,candyTTR);
+			((AnimatedSprite)face).animate(100,true);
 			break;
-		case 1:
-			face = new Sprite(row,column,mCatFaceTextureRegion);
+		case CAT: // done
+			face = new AnimatedSprite(column,row,catTTR);
+			((AnimatedSprite)face).animate(100,true);
 			break;
-		case 2:
-			face = new Sprite(row,column,mBoxFaceTextureRegion);
+		case BOX:
+			face = new Sprite(column,row,boxTR);
 			break;
-		case 5:
-			face = new Sprite(row,column,mWallFaceTextureRegion);
+		case BOMB:
+			face = new AnimatedSprite(column,row,bombTTR);
+			((AnimatedSprite)face).animate(100,true);
 			break;
-		case 6:
-			face = new Sprite(row,column,mInertiaFaceTextureRegion);
+		case ENEMY:
+			face = new AnimatedSprite(column,row,enemyTTR);
+			((AnimatedSprite)face).animate(100,true);
 			break;
-		default:
-			face = new Sprite(row,column,mCandyFaceTextureRegion);
+		case MOVABLE_WALL:
+			face = new Sprite(column,row,movableWallTR);
+			break;
+		case INERTIA_WALL:
+			face = new Sprite(column,row,inertiaWallTR);
+			break;
+		default: // done
+			face = new AnimatedSprite(column,row,candyTTR);
+			((AnimatedSprite)face).animate(100,true);
 			break;
 		}
 		mScene.attachChild(face);
