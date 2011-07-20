9af3dc929cd721b298a8945ed458bce7c6ff030e
shrav mehta <shravvmehtaa@gmail.com>
2011-07-19 17:01:11 -0700

    Crushed Images added Droid Monitor Project

---
+++ Droid Monitor/src/com/embedstudios/droidmonitor/main.java
@@ -0,0 +1,13 @@
+package com.embedstudios.droidmonitor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
