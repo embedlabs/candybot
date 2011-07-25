84229031ec4c40b666a3650bb3f081611b74716b
Shrav Mehta <shravvmehtaa@gmail.com>
2011-07-25 14:00:48 -0700

    Added my test device and changed height and width of the background.tsx

--- CandyCat/assets/tmx/background.tsx
+++ CandyCat/assets/tmx/background.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Background" tilewidth="64" tileheight="64" spacing="3" margin="2">
-	<image source="gfx/bg_tileset.png" width="256" height="192" />
+	<image source="gfx/bg_tileset.png" width="269" height="202" />
 	<tile id="0">
 		<properties>
 			<property name="wall" value="true" />
