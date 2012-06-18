7a5e37efdd5d592d66f650f9163ec0d793384762
Prem Nair <prem.q.nair@gmail.com>
2012-06-18 11:17:25 -0700

    Cleaned up more stuff sorry but do not reclone just clean all projects.

--- gree-app-sdk-android-3.0.0-20120522/vendor/gson/src/net/gree/vendor/com/google/gson/annotations/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package provides annotations that can be used with {@link com.google.net.gree.gson.Gson}.
- * 
- * @author Inderjeet Singh, Joel Leitch
- */
-package net.gree.vendor.com.google.gson.annotations;
--- gree-app-sdk-android-3.0.0-20120522/vendor/signpost/signpost-core/src/test/java/net/gree/oauth/signpost/mocks/OAuthProviderMock.java
+++
@@ -1,7 +0,0 @@
-package net.gree.oauth.signpost.mocks;
-
-public interface OAuthProviderMock {
-
-    void mockConnection(String responseBody) throws Exception;
-
-}
