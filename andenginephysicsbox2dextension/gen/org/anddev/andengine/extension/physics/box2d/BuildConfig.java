ad4a1ea9b399ea15beaf865d7542238bd79c2e8a
Prem Nair <prem.q.nair@gmail.com>
2012-06-22 14:33:00 -0700

    Removing physics.

--- andenginephysicsbox2dextension/gen/org/anddev/andengine/extension/physics/box2d/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.anddev.andengine.extension.physics.box2d;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
