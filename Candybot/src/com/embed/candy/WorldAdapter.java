40f05cf0054f1790113b9bfae0af856a98f24792
Shrav Mehta <shravvmehtaa@gmail.com>
2012-06-30 22:04:00 -0700

    Ops, Forgot to commit this. Gallery World Names and Star Nums (waiting for your parser code perm)

--- Candybot/src/com/embed/candy/WorldAdapter.java
+++ Candybot/src/com/embed/candy/WorldAdapter.java
@@ -4,16 +4,19 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.AbsoluteLayout;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class WorldAdapter extends BaseAdapter {
 	private final Context mContext;
 	private static int position;
+	private int totalStars;
 
 	public static final Integer[] imageIDs = { R.drawable.box1, R.drawable.box2, R.drawable.box3 };
-
+	public static final String[] worldName = {"Easy", "Better", "Medium"};
 	public WorldAdapter(final Context c) {
 		mContext = c;
 	}
@@ -41,17 +44,45 @@ public class WorldAdapter extends BaseAdapter {
 		return position;
 	}
 
+	public void calcStars() {
+		// Calculate stars here premy!
+		totalStars = 1;
+	}
+	
 	@Override
 	public View getView(final int position, final View convertView, final ViewGroup parent) {
 
+		final RelativeLayout border_rl = new RelativeLayout(mContext); // Add ImageView to relative layout
+		
+		RelativeLayout.LayoutParams ls = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		ls.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		
+		final TextView t = new TextView(mContext);
+		t.setText(worldName[position]);
+		t.setTextSize(30);
+		CandyUtils.setMainFont(t);
+		border_rl.addView(t, ls);
+		
+		RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(350,350);
+		lp.setMargins(0, 100, 0, 0);
+		
 		final ImageView i = new ImageView(mContext);
 		i.setImageResource(imageIDs[position]);
 		i.setScaleType(ImageView.ScaleType.FIT_XY);
-		i.setLayoutParams(new LayoutParams(350, 350)); // To fill screen (view)
+		i.setLayoutParams(new LayoutParams(300, 300)); // To fill screen (view)
+		border_rl.addView(i, lp);
+		
+		RelativeLayout.LayoutParams lr = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+		lr.setMargins(0, 470, 0, 0);
+		lr.addRule(RelativeLayout.CENTER_HORIZONTAL);
 
-		final RelativeLayout border_rl = new RelativeLayout(mContext); // Add ImageView to relative layout
-		border_rl.addView(i);
 
+		final TextView a = new TextView(mContext);
+		a.setText(totalStars + "/60");
+		a.setTextSize(12);
+		CandyUtils.setMainFont(a);
+		border_rl.addView(a, lr);
+		
 		return border_rl;
 	}
 }
