0224dd943c70a6635319beff110104f0add6c9df
Prem Nair <prem.q.nair@gmail.com>
2012-07-03 00:13:36 -0700

    Sleeping, will finish in the morning the stats.

--- Candybot/src/com/embed/candy/StatisticsActivity.java
+++ Candybot/src/com/embed/candy/StatisticsActivity.java
@@ -1,5 +1,8 @@
 package com.embed.candy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,11 +13,27 @@ public class StatisticsActivity extends ListActivity {
 	public void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.stats);
-		((TextView)findViewById(R.id.stats_title)).setTypeface(CandyUtils.mainFont);
-		setListAdapter(new StatisticsAdapter(this,fetchStats()));
+		((TextView) findViewById(R.id.stats_title)).setTypeface(CandyUtils.mainFont);
+		setListAdapter(new StatisticsAdapter(this, fetchStats()));
 	}
 
 	public String[] fetchStats() {
-		return new String[]{"yo","yo","yo","yo","yo","yo"};
+		final List<String> stats = new ArrayList<String>();
+		final List<int[][]> worlds = new ArrayList<int[][]>();
+		for (int i = 1;i<=5;i++) {
+			worlds.add(CandyUtils.readLines("world" + i + ".cls", this));
+		}
+
+		// TIME PLAYED
+		// MOVES TAKEN
+		// NUMBER OF RESTARTS
+		// LEVELS COMPLETED
+		// WORLDS COMPLETED
+		// STARS COLLECTED
+		// ACHIEVEMENTS AWARDED
+		// NUMBER OF DEATHS
+		// ENEMIES KILLED
+
+		return stats.toArray(new String[stats.size()]);
 	}
 }
