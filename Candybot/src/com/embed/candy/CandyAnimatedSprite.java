c6a8cec6cdb09c8d21bb075a80ad3053f18e9879
Prem Nair <prem.q.nair@gmail.com>
2012-07-05 21:32:11 -0700

    Last commit of the day. Clean up.

--- Candybot/src/com/embed/candy/CandyAnimatedSprite.java
+++ Candybot/src/com/embed/candy/CandyAnimatedSprite.java
@@ -190,6 +190,9 @@ public class CandyAnimatedSprite extends AnimatedSprite {
 		lastDirectionalMove = 0;
 		stopAnimation();
 		setPosition(initialColumn * 64, initialRow * 64);
+		ppe.reset();
+		ppe.setCenter(initialColumn * 64 + 16, initialRow * 64 + 16);
+		ps.setParticlesSpawnEnabled(false);
 		objectArray[index][CandyEngine.ROW] = initialRow;
 		objectArray[index][CandyEngine.COLUMN] = initialColumn;
 		setCurrentTileIndex(0);
