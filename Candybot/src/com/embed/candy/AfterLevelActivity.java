6ea3494fc461512ce8bed93d3bca3c709a04aefd
Prem Nair <prem.q.nair@gmail.com>
2012-07-06 21:55:43 -0700

    Improved AfterLevelActivity.

--- Candybot/src/com/embed/candy/AfterLevelActivity.java
+++ Candybot/src/com/embed/candy/AfterLevelActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,7 +16,11 @@ public class AfterLevelActivity extends BetterSwarmActivity implements View.OnCl
 
 	TextView al_title, text_moves, text_time;
 	ImageView al_star3, al_star2;
-	Button next_level, return_al;
+	Button next_level, return_al, retry_level, prefs;
+
+	int world;
+	int level;
+	String theme;
 
 	@Override
 	public void onCreate(final Bundle savedInstanceState) {
@@ -39,8 +42,10 @@ public class AfterLevelActivity extends BetterSwarmActivity implements View.OnCl
 		text_time = (TextView) findViewById(R.id.text_time);
 		next_level = (Button) findViewById(R.id.button_next_level);
 		return_al = (Button) findViewById(R.id.button_return_al);
-		CandyUtils.setMainFont(al_title, text_moves, text_time, next_level, return_al);
-		CandyUtils.setClick(this, next_level, return_al);
+		retry_level = (Button) findViewById(R.id.button_retry_level);
+		prefs = (Button) findViewById(R.id.button_preferences);
+		CandyUtils.setMainFont(al_title, text_moves, text_time, next_level, return_al, retry_level, prefs);
+		CandyUtils.setClick(this, next_level, return_al, retry_level, prefs);
 
 		switch (stars) {
 		case 1:
@@ -70,22 +75,32 @@ public class AfterLevelActivity extends BetterSwarmActivity implements View.OnCl
 		}
 
 		if (getIntent().getIntExtra("com.embed.candy.level", 1) == 20) {
-			((LinearLayout) findViewById(R.id.ll_al_buttons)).removeView(findViewById(R.id.button_next_level));
+			next_level.setEnabled(false);
+			next_level.setBackgroundDrawable(CandyUtils.convertToGrayscale(next_level.getBackground().mutate()));
 		}
+
+		world = getIntent().getIntExtra("com.embed.candy.world", 1);
+		level = getIntent().getIntExtra("com.embed.candy.level", 1);
+		theme = getIntent().getStringExtra("com.embed.candy.theme");
+		al_title.setText(getString(R.string.level_complete1) + " " + world + getString(R.string.level_complete2) + level + " " + getString(R.string.level_complete3));
 	}
 
 	@Override
 	public void onClick(final View v) {
 		switch (v.getId()) {
 		case R.id.button_next_level:
-			startActivity(new Intent(this, CandyLevelActivity.class)
-			.putExtra("com.embed.candy.world", getIntent().getIntExtra("com.embed.candy.world", 1))
-			.putExtra("com.embed.candy.level", getIntent().getIntExtra("com.embed.candy.level", 1) + 1)
-			.putExtra("com.embed.candy.theme", getIntent().getStringExtra("com.embed.candy.theme")));
+			startActivity(new Intent(this, CandyLevelActivity.class).putExtra("com.embed.candy.world", world).putExtra("com.embed.candy.level", level + 1).putExtra("com.embed.candy.theme", theme));
+			finish();
+			break;
+		case R.id.button_retry_level:
+			startActivity(new Intent(this, CandyLevelActivity.class).putExtra("com.embed.candy.world", world).putExtra("com.embed.candy.level", level).putExtra("com.embed.candy.theme", theme));
 			finish();
 			break;
 		case R.id.button_return_al:
 			finish();
+			break;
+		case R.id.button_preferences:
+			startActivity(new Intent(this, CandyPreferenceActivity.class));
 			break;
 		}
 	}
