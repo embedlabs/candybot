edafa758d6e3e40d543822bf56fe94784b8fbdc1
Prem Nair <prem.q.nair@gmail.com>
2012-07-25 15:58:23 -0700

    Code cleanup.

--- Candybot/src/com/embed/candy/StatisticsActivity.java
+++ Candybot/src/com/embed/candy/StatisticsActivity.java
@@ -18,6 +18,7 @@ import android.widget.TextView;
 
 import com.embed.candy.adapter.StatisticsAdapter;
 import com.embed.candy.save.SaveIO;
+import com.embed.candy.swarmservice.CandyAchievements;
 import com.embed.candy.util.ViewUtils;
 import com.swarmconnect.Swarm;
 
@@ -36,14 +37,15 @@ public class StatisticsActivity extends ListActivity {
 
 	public String[] fetchStats() {
 		final List<String> stats = new ArrayList<String>();
-		final List<int[][]> worlds = new ArrayList<int[][]>();
-		for (int i = 1; i <= 5; i++) {
-			worlds.add(SaveIO.readLines("world" + i + ".cls", this));
+		final List<int[][]> allWorlds = new ArrayList<int[][]>();
+		for (int i=1;i<=5;i++) {
+			allWorlds.add(SaveIO.readLines("world"+i+".cls", this));
 		}
+		final int[][][] worlds = allWorlds.toArray(new int[5][][]);
 		final DecimalFormat f = new DecimalFormat("#,###");
 
 		// TIME PLAYED
-		int seconds = statisticObtainer(worlds, TOTAL_TIME_MILLIS) / 1000;
+		int seconds = CandyAchievements.statisticObtainer(worlds, TOTAL_TIME_MILLIS) / 1000;
 		final int hours = seconds / 3600; // number of hours floored
 		seconds -= (3600 * hours); // subtract to get remaining seconds
 		final int minutes = seconds / 60; // number of minutes remaining
@@ -51,11 +53,11 @@ public class StatisticsActivity extends ListActivity {
 		stats.add(getString(R.string.stats_time_played) + ": " + ((hours > 0) ? hours + "h " : "") + minutes + "m " + seconds + "s");
 
 		// MOVES TAKEN
-		final int moves = statisticObtainer(worlds, TOTAL_MOVES);
+		final int moves = CandyAchievements.statisticObtainer(worlds, TOTAL_MOVES);
 		stats.add(getString(R.string.stats_moves_taken) + ": " + f.format(moves));
 
 		// NUMBER OF RESTARTS
-		final int restarts = statisticObtainer(worlds, TOTAL_RESTARTS);
+		final int restarts = CandyAchievements.statisticObtainer(worlds, TOTAL_RESTARTS);
 		stats.add(getString(R.string.stats_total_restarts) + ": " + f.format(restarts));
 
 		// LEVELS COMPLETED
@@ -85,33 +87,26 @@ public class StatisticsActivity extends ListActivity {
 		stats.add(getString(R.string.stats_worlds_completed)+": "+worldsCompleted);
 
 		// STARS EARNED
-		final int stars = statisticObtainer(worlds, STATUS);
+		final int stars = CandyAchievements.statisticObtainer(worlds, STATUS);
 		stats.add(getString(R.string.stats_stars_earned) + ": " + stars);
 
 		// ACHIEVEMENTS AWARDED
-		// TODO
+		final int achievements = CandyAchievements.achievementCount();
+		stats.add(getString(R.string.stats_achievements_awarded)+": "+achievements);
 
 		// NUMBER OF DEATHS
-		final int deaths = statisticObtainer(worlds, TOTAL_DEATHS);
+		final int deaths = CandyAchievements.statisticObtainer(worlds, TOTAL_DEATHS);
 		stats.add(getString(R.string.stats_number_of_candybot_deaths) + ": " + f.format(deaths));
 
 		// CANDIES BURNED
-		final int burned = statisticObtainer(worlds, TOTAL_BURNS);
+		final int burned = CandyAchievements.statisticObtainer(worlds, TOTAL_BURNS);
 		stats.add(getString(R.string.stats_candies_burned) + ": " + f.format(burned));
 
 		// ENEMIES DEFEATED
-		final int enemies = statisticObtainer(worlds, TOTAL_DEFEATED);
+		final int enemies = CandyAchievements.statisticObtainer(worlds, TOTAL_DEFEATED);
 		stats.add(getString(R.string.stats_enemies_defeated) + ": " + f.format(enemies));
 
 		return stats.toArray(new String[stats.size()]);
-	}
-
-	public int statisticObtainer(final List<int[][]> worlds, final int index) {
-		int temp = 0;
-		for (int[][] world : worlds) {
-			temp += world[20][index];
-		}
-		return temp;
 	}
 
 	@Override
